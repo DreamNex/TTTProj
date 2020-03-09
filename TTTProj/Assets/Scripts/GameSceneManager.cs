75a5364936d6c598af330c05374e5675399ee3b4
Isa Hong Xi <dreamwalker56@gmail.com>
2020-03-09 12:02:50 +0800

    Included a release folder
    Has a build ready inside

---
+++ TTTProj/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public static GameSceneManager instance;
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SetScene(string str)
+    {
+        SceneManager.LoadScene(str);
+    }
+
+    public void SetScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void SetSceneWithData(int gamemode)
+    {
+        go.GetComponent<GameData>().SetGameMode(gamemode);
+        if(gamemode == 4)
+        {
+            go.GetComponent<GameData>().PlayComputer = true;
+        }
+    }
+
+    public Scene GetCurrentScene()
+    {
+        return SceneManager.GetActiveScene();
+    }
+
+    public void QuiteGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ TTTProj/Assets/Scripts/SelectButtonEvent.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButtonEvent : MonoBehaviour
+{
+    private Button btn;
+    private GameObject Data_obj;
+    private GameObject SManager;
+    private Sprite symbol1;
+    private Sprite symbol2;
+    private Sprite symbol3;
+    private Sprite symbol4;
+
+    public GameObject imge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SManager = GameObject.FindGameObjectWithTag("SceneManager");
+        Data_obj = GameObject.FindGameObjectWithTag("GameData");
+
+        symbol1 = Resources.Load<Sprite>("ui/symbol-chip");
+        symbol2 = Resources.Load<Sprite>("ui/symbol-club");
+        symbol3 = Resources.Load<Sprite>("ui/symbol-diamond");
+        symbol4 = Resources.Load<Sprite>("ui/symbol-heart");
+
+        btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(ClickEvent);
+    }
+
+    void ClickEvent()
+    {
+        //Set the sprite to the game data
+        if (!Data_obj.GetComponent<GameData>().PlayComputer)
+        {
+            if (Data_obj.GetComponent<GameData>().GetPlayer1Img() == null)
+            {
+                Data_obj.GetComponent<GameData>().SetPlayer1Img(imge.GetComponent<Image>().sprite);
+            }
+            else
+            {
+                Data_obj.GetComponent<GameData>().SetPlayer2Img(imge.GetComponent<Image>().sprite);
+            }
+            this.GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            if (Data_obj.GetComponent<GameData>().GetPlayer1Img() == null)
+            {
+                Data_obj.GetComponent<GameData>().SetPlayer1Img(imge.GetComponent<Image>().sprite);
+                if(imge.GetComponent<Image>().sprite == symbol1)
+                {
+                    Data_obj.GetComponent<GameData>().SetPlayer2Img(symbol2);
+                }
+                else
+                {
+                    Data_obj.GetComponent<GameData>().SetPlayer2Img(symbol1);
+                }
+            }
+        }
+    }
+}
